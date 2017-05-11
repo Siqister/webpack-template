d59f70157a9f398518641a6350c3d3a02fe57930
siqister <siqi.zhu@gmail.com>
2017-05-11 16:38:36 -0400

    Upgrade to webpack 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as d3 from 'd3';
+import 'bootstrap/dist/css/bootstrap.css';
 
 console.log(d3);
 console.log('Starting index.js');
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,41 @@
 //run webpack-dev-server --content-base dist
 //https://webpack.github.io/docs/configuration.html
+var path = require('path');
 
 module.exports = {
 	entry:'./src/index.js',
 	output:{
-		path:__dirname+'/dist',
+		path: path.resolve(__dirname,'dist'),
 		filename:'bundle.js',
 		publicPath:'/assets/'
 	},
 	devtool:"#source-map",
 	devServer:{
-		contentBase:'./dist'
+		contentBase: path.join(__dirname,'dist')
 	},
 	module:{
-		loaders:[
+		rules:[
 			{
 				test: /\.jsx?$/,
-				loader:'babel-loader',
-				query:{
-					presets:['es2015','react']
-				},
-				include:__dirname+'/src'
+				include: path.resolve(__dirname,'src'),
+				use:{
+					loader:'babel-loader',
+					options:{
+						presets:['es2015','react']
+					}
+				}
 			},
-			{ test: /\.json$/,loader:"json-loader"},
-			{ test: /\.css$/, loader:"style-loader!css-loader"},
-			{ test: /\.woff(\d+)?$/, loader: 'url?prefix=font/&limit=5000&mimetype=application/font-woff' },
-         	{ test: /\.ttf$/, loader: 'file?prefix=font/' },
-         	{ test: /\.eot$/, loader: 'file?prefix=font/' },
-         	{ test: /\.svg$/, loader: 'file?prefix=font/' },
+			{ 
+				test: /\.css$/, 
+				use:[
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{ test: /\.woff(\d+)?$/, loader: 'url-loader?prefix=font/&limit=5000&mimetype=application/font-woff' },
+         	{ test: /\.ttf$/, loader: 'file-loader?prefix=font/' },
+         	{ test: /\.eot$/, loader: 'file-loader?prefix=font/' },
+         	{ test: /\.svg$/, loader: 'file-loader?prefix=font/' },
          	{ test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff"},
          	{ test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
 		]
