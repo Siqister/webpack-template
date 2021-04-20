5ab974f6d4e9c4e10a1e1061a02889dbb826b5b0
siqister <siqi.zhu@gmail.com>
2021-04-20 14:30:28 -0400

    Update webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -27,17 +27,13 @@ module.exports = {
 					'css-loader'
 				]
 			},
-			// { test: /\.woff(\d+)?$/, loader: 'url-loader?prefix=font/&limit=5000&mimetype=application/font-woff' },
-   //    { test: /\.ttf$/, loader: 'file-loader?prefix=font/' },
-   //    { test: /\.eot$/, loader: 'file-loader?prefix=font/' },
-   //    { test: /\.svg$/, loader: 'file-loader?prefix=font/' },
-   //   	{ test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff"},
-   //   	{ test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource'
+			},
      	{
      		test: /\.(png|svg|jpg|gif)$/,
-     		use: {
-     			loader: 'file-loader'
-     		}
+     		type: 'asset/resource'
      	},
      	{
      		test: /\.html$/,
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,6 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = merge(common, {
